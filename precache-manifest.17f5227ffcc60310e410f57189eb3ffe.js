20706b0576bc5202e59fec6e952af1af79b1a341
sinfounad <ivanpin971@hotmail.com>
2020-06-22 21:50:12 -0500

    Updates

---
+++ precache-manifest.17f5227ffcc60310e410f57189eb3ffe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a37fb42dde6ad8b0d48f4c6cabd3658e",
+    "url": "/testReact/index.html"
+  },
+  {
+    "revision": "9df96e1287180fb21beb",
+    "url": "/testReact/static/css/main.8d68afd0.chunk.css"
+  },
+  {
+    "revision": "1d7913968bf6a2d6170a",
+    "url": "/testReact/static/js/2.fb763b58.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/testReact/static/js/2.fb763b58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9df96e1287180fb21beb",
+    "url": "/testReact/static/js/main.4ce26aa7.chunk.js"
+  },
+  {
+    "revision": "359e8a16473780bdf17e",
+    "url": "/testReact/static/js/runtime-main.f819c0a9.js"
+  },
+  {
+    "revision": "91e96f67ec03b3945db0ae16f0aff950",
+    "url": "/testReact/static/media/LogoJunta.91e96f67.jpeg"
+  },
+  {
+    "revision": "0290c48b6e6bc15114c86f84b6ea00cb",
+    "url": "/testReact/static/media/principal1.0290c48b.jpeg"
+  }
+]);
